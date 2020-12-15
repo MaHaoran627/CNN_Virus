65cc84f1c6c23d5d1ce3197543eab5cf643cbb12
627oldcat <603719821@qq.com>
2020-12-15 13:33:23 +0800

    update script for 150-mer evaluation

--- evaluation_on_simulated_150mers.py
+++ evaluation_on_simulated_150mers.py
@@ -10,18 +10,18 @@ from sklearn.metrics import cohen_kappa_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
-from preprocessing import get_kmer_from_100mer,get_params_100mer,DataGenerator_from_100mer
+from preprocessing import get_kmer_from_150mer,get_params_150mer,DataGenerator_from_150mer
 from voting import get_final_result
 
 #path for testing file
-filepath_train="./data/100mer_testing"
+filepath_train="./data/ICTV_150mer_benchmarking"
 #path for trained model
 filepath_model="./data/pretrained_model.h5"
 
 d_nucl={"A":0,"C":1,"G":2,"T":3,"N":4}
-f_matrix,y_true,f_pos=get_kmer_from_100mer(filepath_train)
-params=get_params_100mer()
-testing_generator = DataGenerator_from_100mer(f_matrix, **params)
+f_matrix,y_true,f_pos=get_kmer_from_150mer(filepath_train)
+params=get_params_150mer()
+testing_generator = DataGenerator_from_150mer(f_matrix, **params)
 
 model=load_model(filepath_model)
 hist = model.predict_generator(testing_generator,
@@ -35,9 +35,9 @@ predicted_loc_prob_list=[max(i) for i in hist[1]]
 
 final_label=[]
 final_loc=[]
-num_iters=int(len(predicted_labels_list)*1.0/51)
+num_iters=int(len(predicted_labels_list)*1.0/101)
 for i in range(0,num_iters):
-	tmp_label,tmp_loc=get_final_result(predicted_labels_list[i*51:(i+1)*51],predicted_prob_list[i*51:(i+1)*51],predicted_loc_list[i*51:(i+1)*51],predicted_loc_prob_list[i*51:(i+1)*51])
+	tmp_label,tmp_loc=get_final_result(predicted_labels_list[i*101:(i+1)*101],predicted_prob_list[i*101:(i+1)*101],predicted_loc_list[i*101:(i+1)*101],predicted_loc_prob_list[i*101:(i+1)*101])
 	final_label.append(str(tmp_label))
 	final_loc.append(str(tmp_loc))
 
